1afda4fa36bb3e175b40fcbd1eaca940b7a43666
Karn Wong <karn@karnwong.me>
2024-07-13 17:57:05 +0700

    set rate limiting

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,9 +12,17 @@ import (
 func main() {
 	app := fiber.New()
 
+	// 3 requests per 10 seconds max
+	app.Use(limiter.New(limiter.Config{
+		Expiration: 10 * time.Second,
+		Max:        3,
+	}))
+
+	// route
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
+	// entrypoint
 	log.Fatal(app.Listen(":3000"))
 }
